4f2163265c11b2d5cd9694a30a1a8b3d9c22038b
Randy Whitley <randywhitley84@gmail.com>
2021-02-24 18:50:37 -0600

    added compents folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
+import "./App.css";
 
 function App() {
   return (
