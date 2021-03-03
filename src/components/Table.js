4153e37c9ffe3b3a22faaaad39f4a0d7558b8dd7
Randy Whitley <randywhitley84@gmail.com>
2021-03-02 22:17:11 -0600

    anvk

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,42 +1,42 @@
-import React, { useContext } from "react";
-import BodyEl from "./BodyEl";
-import "../styles/Table.css";
-import DataAreaContext from "../utils/DataAreaContext";
+// import React, { useContext } from "react";
+// import BodyEl from "./BodyEl";
+// import "../styles/Table.css";
+// import DataAreaContext from "../utils/DataAreaContext";
 
-const Table = () => {
-  const context = useContext(DataAreaContext);
+// const Table = () => {
+//   const context = useContext(DataAreaContext);
 
-  return (
-    <div className="datatable mt-5">
-      <table
-        id="table"
-        className="table table-striped table-hover table-condensed"
-      >
-        <thead>
-          <tr>
-            {context.developerState.headings.map(({ name, width }) => {
-              return (
-                <th
-                  className="col"
-                  key={name}
-                  style={{ width }}
-                  onClick={() => {
-                    // context.handleSort(name.toLowerCase());
-                    context.handleSort(name);
-                  }}
-                >
-                  {name}
-                  <span className="pointer"></span>
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
+//   return (
+//     <div className="datatable mt-5">
+//       <table
+//         id="table"
+//         className="table table-striped table-hover table-condensed"
+//       >
+//         <thead>
+//           <tr>
+//             {context.developerState.headings.map(({ name, width }) => {
+//               return (
+//                 <th
+//                   className="col"
+//                   key={name}
+//                   style={{ width }}
+//                   onClick={() => {
+//                     // context.handleSort(name.toLowerCase());
+//                     context.handleSort(name);
+//                   }}
+//                 >
+//                   {name}
+//                   <span className="pointer"></span>
+//                 </th>
+//               );
+//             })}
+//           </tr>
+//         </thead>
 
-        <BodyEl />
-      </table>
-    </div>
-  );
-};
+//         <BodyEl />
+//       </table>
+//     </div>
+//   );
+// };
 
-export default Table;
+// export default Table;
