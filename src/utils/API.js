c8deb1ffbd366babc769b5f19f320a92787f8323
Randy Whitley <randywhitley84@gmail.com>
2021-03-02 22:19:21 -0600

    fdhsjkafsdhkjfsda

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default {
-// https://randomuser.me/ 
-  getUsers: function () {
-    return axios.get("https://randomuser.me/api/?results=10&nat=us");
-  },
-};
+// export default {
+// // https://randomuser.me/ 
+//   getUsers: function () {
+//     return axios.get("https://randomuser.me/api/?results=10&nat=us");
+//   },
+// };
