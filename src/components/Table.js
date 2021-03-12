0a10f690979014055cd3eb1ce70a3d960a219251
Randy Whitley <randywhitley84@gmail.com>
2021-03-11 18:21:52 -0600

    added public folder

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,38 +3,40 @@ import BodyEl from "./BodyEl";
 import "../styles/Table.css";
 import DataAreaContext from "../utils/DataAreaContext";
 
-function Table() {
+const Table = () => {
   const context = useContext(DataAreaContext);
-  console.log(context);
-useEffect (()=>{console.log(context)},[])
-  return ( <div></div>
-    // <div className="datatable mt-5">
-    //   <table id="table" className="table table-striped table-hover table-condensed">
-    //     <thead>
-    //       <tr>
-    //         {context.developerState.headings.map(({ name, width }) => {
-    //           return (
-    //             <th
-    //               className="col"
-    //               key={name}
-    //               style={{ width }}
-    //               onClick={() => {
-    //                 context.handleSort(name.toLowerCase());
-    //                 // context.handleSort(name);
-    //               }}
-    //             >
-    //               {name}
-    //               <span className="pointer"></span>
-    //             </th>
-    //           );
-    //         })}
-    //       </tr>
-    //     </thead>
 
-    //     <BodyEl />
-    //   </table>
-    // </div>
+  return (
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="table table-striped table-hover table-condensed"
+      >
+        <thead>
+          <tr>
+            {context.developerState.headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                   
+                    context.handleSort(name);
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+
+        <BodyEl />
+      </table>
+    </div>
   );
-}
+};
 
 export default Table;
